231e3ba7b2518fe5dc8ba28fb7612d0e119cb453
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-09-27 02:17:15 +0530

    Update token expiry defaults in user model to ensure fallback values are set

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -89,7 +89,7 @@ userSchema.methods.generateAccessToken = function () {
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "1d",
     }
   );
 };
@@ -100,7 +100,7 @@ userSchema.methods.generateRefreshToken = function () {
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "10d",
     }
   );
 };
