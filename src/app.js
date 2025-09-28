ffe47988bc40de9fc40060087d2ad139d3709d42
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-09-28 11:49:04 +0530

    Enable dotenv configuration in app.js to load environment variables for improved application setup.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-// import dotenv from "dotenv";
-// dotenv.config({
-//   path: "./.env",
-// });
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 
 import express from "express";
 import cors from "cors";
